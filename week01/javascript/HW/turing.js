6a4f60ee9ac555c16101ba93ea6eabde89d8743e
einar <einarzb@gmail.com>
2017-06-08 15:34:44 +0300

    turing

--- week01/javascript/HW/turing.js
+++ week01/javascript/HW/turing.js
@@ -9,62 +9,61 @@ this code is written by Einar Barzilay
 var readlineSync = require('readline-sync');
 
 //initialize
-var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-"A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V","X","Y","Z", ""
-];
-var cba = [];
 
+function getAbc(){
+  return 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+  "A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V","X","Y","Z", "";
+}
 //main function to create key
-function turingMachine() {
-  var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-  "A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V","X","Y","Z", ""
- ];
+function turingMachine(array) {
+  var cba = [];
   var temp;
   var randomize;
-        for (var i = 0; i < abc.length; ++i) {
+        for (var i = 0; i < array.length; ++i) {
                //each char in the ABC array is reassigned with random number
                 randomize = (Math.floor(Math.random() * abc.length)); //the length of the ABC array.
-                temp = abc[i]; //saves the ordered ABC
-                abc[i] = abc[randomize]; // each char is getting scrumbbled
-                abc[randomize] = temp;
+                temp = array[i]; //saves the ordered ABC
+                array[i] = array[randomize]; // each char is getting scrumbbled
+                array[randomize] = temp;
             }
           //assign the mixed array and return it
-            cba = abc;
-            return cba;
+            cba = array;
+            encrypt(array, cba);
+            return array;
   }
 
 // encrypt function - gets data from user and encrypt it by the key I've made up
-function encrypt(abc, cba){
+function encrypt(array, cba){
   var encryptWord = " ";
   var userInput = readlineSync.question('Please enter word > ');
 
   //loop through users input
   for (var i = 0; i < userInput.length; ++i) {
     //nested loop through the ORDERED array
-    for (var j=0; j < abc.length; ++j){
+    for (var j=0; j < array.length; ++j){
       //if letter of the users input exists in the ordered ABC array
-      if(userInput[i] == abc[j]) {
+      if(userInput[i] == array[j]) {
         //then 'push' onto encryptWord the opposite letter thats inside the UNORDERD ARRAY
          encryptWord += cba[j];
       }
     } //nested loop
   } // for loop
   console.log("\n Encrypted word " + userInput + " is now >> " + encryptWord + "\n Under his eye!");
-  decrypt(encryptWord, abc, cba);
+  decrypt(encryptWord, array, cba);
   return encryptWord;
 }
 
 //decrypt back
-function decrypt(encryptWord, abc, cba){
+function decrypt(encryptWord, array, cba){
   var decryptWord = " ";
   //loop through the encryptWord
   for (var i = 0; i < encryptWord.length; ++i) {
     //nested loop through the ORDERED array
-      for (var j=0; j < abc.length; ++j){
+      for (var j=0; j < array.length; ++j){
         //if letter is inside the UNORDERED array
         if(encryptWord[i] == cba[j]) {
           //then 'push' onto decryptWord the letter thats inside the ORDERD ARRAY
-          decryptWord += abc[j];
+          decryptWord += array[j];
           }
         } //nested loop
   }//end loop
@@ -73,5 +72,4 @@ function decrypt(encryptWord, abc, cba){
 }
 
 //invoke functions
-turingMachine();
-encrypt(abc, cba);
+var key = turingMachine(getAbc);
