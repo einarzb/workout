1388cca2d18cbfad8cc77e4fd08de493fd97b0be
einar <einarzb@gmail.com>
2017-06-08 16:58:45 +0300

    split exe

--- week01/javascript/HW/split.js
+++ week01/javascript/HW/split.js
@@ -15,22 +15,32 @@ var arr = strSplit("abbcbcbbcbcbccbbddd", 'c');
 var readlineSync = require('readline-sync');
 
 //convert user's input (string) onto an array
-function strSplit () {
+function strSplit (str, splitChar) {
     //initial
+    var tempChar;
     var arr = []; //empty array that will store the splitted string
-    var userInput = readlineSync.question('Please enter a text to split > ');
-    var splitChar = readlineSync.question('Please enter a char to split by > ');
+
+    // var userInput = readlineSync.question('Please enter a text to split > ');
+    // var splitChar = readlineSync.question('Please enter a char to split by > ');
+    console.log(str);
 
     //rockenroll
-    for (var i = 0; i < userInput.length; ++i) {
-          //if you reach
-          if(userInput[i] == splitChar) {
-            arr += userInput[i];
+    for (var i = 0; i < str.length; ++i) {
+          //if you find the dividor char push the char to the array
+          if(str[i] == splitChar) {
+            console.log(str[i]);
+              str[i] = " " //swap other chars with " "
+              console.log(str[i]);
+
+              // arr += str[i];
           } else {
-            userInput[i] = " "
+            tempChar = str[i];
+
           }
         }
+        return arr;
         console.log(arr);
   }
 
-strSplit();
+var arr = strSplit("abbcbcbbcbcbccbbddd", 'c');
+//["abb","b","bb","b","b" ,"","bbddd"]
