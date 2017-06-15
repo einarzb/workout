c143573a94ab3b80e9f24e3969ba7b2a6a09e66e
einar <einarzb@gmail.com>
2017-06-15 19:26:35 +0300

    tic tac toe withought winner

--- week02/HW/tictac.js
+++ week02/HW/tictac.js
@@ -4,86 +4,118 @@ var readlineSync = require('readline-sync');
 //ui function
 function ticTacToe(){
   //settings
-  //console.log(player + " choose spot " + move);
-  //var player = readlineSync.question('Please enter your name > ');
-  // console.log("hello " + player + " you will play with X");
+  var player = readlineSync.question('Please enter your name > ');
+  console.log("hello " + player + " you will play with X");
 
+  //2D array
   var board = [[1,2,3],
                [4,5,6],
-               [7,8,9]]; //2D array
+               [7,8,9]];
+
   var turn = 0; //turn counter
-  displayBoard(); //render func
+  displayBoard();
   userTurn();//user would always start FIRST and would be assinged with X
+
+/*************************************** user turn function ******************************************************/
 
   function userTurn() {
       while(turn < 9){
-      var move = readlineSync.questionInt('choose spot on board >');
-      for (var i = 0; i < board.length; i++) {
-          for (var j = 0; j < board.length; j++) {
-            if (move === board[i][j]) {
-                board[i][j] = "x";
-                computerTurn();
-                turn++;
-                break;
+      var move = readlineSync.questionInt(player + ", please choose spot on the board >> " );
+                setXo(move,'X',0)
                 }//end if
              }//end nested for loop
-           }//end for loop
-        }//end while
-      } //end of userTurn function
+
+/*************************************** computer turn function ******************************************************/
 
   function computerTurn() {
-      while(turn < 8) { // 8 b/c user starts first!
-      var computerMove = (Math.floor(Math.random() * 9 + 1)); // 9 + 1 make this start from 0
-      console.log("comp move is " + computerMove);
-      for (var i = 0; i < board.length; i++) {
-          for (var j = 0; j < board.length; j++) {
-            if (computerMove === board[i][j]) {
-                board[i][j] = "O";
-                displayBoard();
-                userTurn();
-                turn++;
-                break;
-            }
+      while(turn < 9) { // 8 b/c user starts first!
+                //var computerMove =(Math.floor(Math.random() * (9 + 1))); // 9 + 1 make this start from 0
+                //console.log("\n computer made a move on cell number: " + computerMove);
+                //setXo(computerMove,'O',1);
+        //      }
+var options=[];
+var k=0;
+  for (var i = 0; i < board.length; i++) {
+  for (var j = 0; j < board.length; j++) {
+        if(!isNaN(board[i][j]))
+        {
+          options[k]=board[i][j];
         }
-      }
-    }
-  } //end of  computerTurn function
+  }
+  }
+  var computerMove = options[(Math.floor(Math.random() * (k-1 + 1)))];
+  console.log("\n computer made a move on cell number: " + computerMove);
 
+setXo(computerMove,'O',1);
+    }
+}
+
+/*************************************** display board function ******************************************************/
 
    function displayBoard(){
-     for (var i = 0; i < board.length; i++) {
+     console.log("-------------------------");
+     for (var i = 0; i < 3  ; i++) {
           console.log(board[i]);
       }
+      console.log("-------------------------");
     }
 
+/*************************************** display board function ******************************************************/
+ function setXo(move,xOrY,checkPlayer){
+     for (var i = 0; i < board.length; i++) {
+         for (var j = 0; j < board.length; j++) {
 
-  return playTicTacToe(ticTacToe);
+           if (move === board[i][j]) {
+               board[i][j] = xOrY;
+               turn++;
+               displayBoard();
+               if(checkPlayer){
+                  userTurn();
+              }
+                else {
+                  computerTurn();
+                }
 
-  function playTicTacToe(ticTacToe){
-    console.log("im playing");
+               //checkWin() if turn is 5
+           }
+       }
+     }
+ }
+   return 0; //not sure about it , me 2
+}
+
+
+
+
+/*************************************** logic function ******************************************************/
+
+function playTicTacToe(ticTacToe){
+    console.log("im playing")
     function newGame(){
         return {
             newGame: newGame,
             play:play
           }
-        }
+
       }
       //invoke
       var game = ticTacToe()
       game.newGame();
       game.play(5);
-}
+
+  }
 
 
 
-//all the algorithem
-function newticTacToe(ticTacToe){
+//would reset the function
+// function resetTicTacToe(ticTacToe){
+//   var board = [[1,2,3],
+//                [4,5,6],
+//                [7,8,9]];
+// console.log(board);/
+//}
+ // displayBoard();
 
-  var player; //string name
-  var computer; //empty obj
-  var playerScore = 0;
-  var computerScore = 0;
-}
 
 //invoke the game
-ticTacToe(newticTacToe());
+ticTacToe();
